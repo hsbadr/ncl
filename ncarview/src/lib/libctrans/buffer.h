df2fdd713b70283d6ed7a2a3cc45a4f449c3b870
Hamada S. Badr <hamada.s.badr@gmail.com>
2020-12-05 06:47:15 -0500

    Fix functions declared implicitly warnings
    wget -q -O - https://src.fedoraproject.org/rpms/ncl/raw/master/f/ncl-5.1.0-includes.patch | patch -p1 -i -

---
+++ ncarview/src/lib/libctrans/buffer.h
@@ -0,0 +1,37 @@
+/*
+ *	$Id: ncl-5.1.0-includes.patch,v 1.1 2009/03/10 14:58:16 orion Exp $
+ */
+/************************************************************************
+*                                                                       *
+* This file is free software; you can redistribute it and/or modify     *
+* it under the terms of the GNU General Public License as published     *
+* by the Free Software Foundation; either version 2 of the License, or  *
+* (at your option) any later version.                                   *
+*                                                                       *
+* This software is distributed in the hope that it will be useful, but  *
+* WITHOUT ANY WARRANTY; without even the implied warranty of            *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
+* General Public License for more details.                              *
+*                                                                       *
+* You should have received a copy of the GNU General Public License     *
+* along with this software; if not, write to the Free Software         *
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307   *
+* USA.                                                                  *
+*                                                                       *
+************************************************************************/
+
+/***********************************************************************
+*                                                                      *
+*                          Copyright (C)  1990                         *
+*            University Corporation for Atmospheric Research           *
+*                          All Rights Reserved                         *
+*                                                                      *
+*                      NCAR View V3.01 - UNIX Release                  *
+*                                                                      *
+***********************************************************************/
+
+#include "ctrandef.h"
+
+int	GcapOpenBuffer(char *file);
+void	flush(void);
+void	buffer(SignedChar *str, int count);
---
+++ ncarview/src/lib/libncarg_ras/misc.h
@@ -0,0 +1,41 @@
+/*
+ *	$Id: ncl-5.1.0-includes.patch,v 1.1 2009/03/10 14:58:16 orion Exp $
+ */
+/************************************************************************
+*                                                                       *
+*                Copyright (C)  2000                                    *
+*        University Corporation for Atmospheric Research                *
+*                All Rights Reserved                                    *
+*                                                                       *
+* This file is free software; you can redistribute it and/or modify     *
+* it under the terms of the GNU General Public License as published     *
+* by the Free Software Foundation; either version 2 of the License, or  *
+* (at your option) any later version.                                   *
+*                                                                       *
+* This software is distributed in the hope that it will be useful, but  *
+* WITHOUT ANY WARRANTY; without even the implied warranty of            *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
+* General Public License for more details.                              *
+*                                                                       *
+* You should have received a copy of the GNU General Public License     *
+* along with this software; if not, write to the Free Software         *
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307   *
+* USA.                                                                  *
+*                                                                       *
+************************************************************************/
+
+/***********************************************************************
+*                                                                      *
+*                          Copyright (C)  1991                         *
+*            University Corporation for Atmospheric Research           *
+*                          All Rights Reserved                         *
+*                                                                      *
+*                                                                      *
+***********************************************************************/
+/*	File:	misc.h
+ */
+
+int read_swap(FILE *fp, int nb, char *buf, int swapflag);
+void _swapshort (register char *bp, register unsigned n);
+void _swaplong (register char *bp, register unsigned n);
+int ImageCount_(char *name, char *format);
